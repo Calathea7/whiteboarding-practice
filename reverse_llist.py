cb088a71d22f063f829e9bc39ae874c2ccbefdd5
Anastasia Correa <correa.anastasia7@gmail.com>
2020-09-26 19:03:17 -0700

    delete given node in llist

--- reverse_llist.py
+++ reverse_llist.py
@@ -56,3 +56,20 @@ def contains_cycle(first_node):
             return True
 
     return False
+
+def delete_node(node_to_delete):
+
+    # Delete the input node from the linked list
+
+    next_node = node_to_delete.next
+
+    if next_node:
+        # Replace the input node's value and pointer with the next
+        # node's value and pointer. The previous node now effectively
+        # skips over the input node
+        node_to_delete.value = next_node.value
+        node_to_delete.next  = next_node.next
+
+    else:
+        # Eep, we're trying to delete the last node!
+        raise Exception("Can't delete the last node with this technique!")
